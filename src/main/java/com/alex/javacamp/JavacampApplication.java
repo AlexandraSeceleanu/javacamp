e38a3e87803a945aebf1481d82240d6b2d431586
AlexandraSeceleanu <80269629+AlexandraSeceleanu@users.noreply.github.com>
2022-05-19 22:08:29 +0300

    initial project

---
+++ src/main/java/com/alex/javacamp/JavacampApplication.java
@@ -0,0 +1,13 @@
+package com.alex.javacamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacampApplication.class, args);
+	}
+
+}
